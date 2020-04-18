badc6bc888aea0129c61a13fdb1209c8c0caf023
tiger0421 <taiger0421@yahoo.co.jp>
2020-04-18 14:04:08 +0900

    get argument

--- launch.sh
+++ launch.sh
@@ -1,11 +1,14 @@
 #!bin/bash
 xhost +local:root
 
-docker run -it --rm --name kinetic \
+CONTAINER_NAME=$1
+
+docker run -it --rm --name ros \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -e DISPLAY=$DISPLAY \
     -e QT_X11_NO_MITSHM=1 \
     --gpus all \
-    ros:kinetic /bin/bash
+    ${CONTAINER_NAME:-tiger0421/ros-gpu-gazebo:1.0-glvnd-runtime-ubuntu18.04} \
+    /bin/bash
 
 xhost -local:root
